6110a3d5a525b0da6cbb71c757b9939b8d02b9dd
Berlin Sohn <berlinsohn@Berlins-MacBook-Pro.local>
2018-04-07 13:10:38 -0600

    added more content

--- src/components/pages/cs-principles-page/subpages/task-seven-subpage/TaskSevenSubpage.js
+++ src/components/pages/cs-principles-page/subpages/task-seven-subpage/TaskSevenSubpage.js
@@ -6,8 +6,82 @@ class TaskSevenSubpage extends Component {
     render() {
         return (
             <div>
-                <p>Evaluate a proposed solution to a computational problem including locating and correcting 
-                    errors and justifying appropriateness and correctness</p>
+                <h1>Evaluate a proposed solution to a computational problem including locating and correcting 
+                    errors and justifying appropriateness and correctness</h1>
+                <p>Dubugging is common practice in my daily job. Often a proposed solution can be found to have 
+                    error once it is implemented and a bug is found. However, some bugs are easier to spot than others 
+                    and some go unnoticed until a user experiences the issue. Making mistakes is unavoidable, however, 
+                    there are ways we can attempt mitigate these oversights in practice. One thing I always try to remember 
+                    when writing new code is to test all scenarios; don't assume there will always be a happy path.</p>
+                <p>Even though there are many ways to solve a problem, the approach you take must correctly use all 
+                    concepts and techniques.</p>
+                <h2>Let's take a look at an example:</h2>
+                <p>Using java, print out the letter grade associated with the inputted int value</p>
+                <p>The original solution was this:</p>
+                <pre>
+{`
+switch (grade)
+{
+case 5:
+    System.out.println("The grade is A.");
+case 4:
+    System.out.println("The grade is B.");
+case 3:
+    System.out.println("The grade is C.");
+case 2:
+    System.out.println("The grade is D.");
+case 1:
+    System.out.println("The grade is F.");
+default:
+    System.out.println("The grade is invalid");
+}
+`}
+                </pre>
+                <p>But can you spot the error?</p>
+                <p>Let's suppose the value of the grade is 4. The value of the grade does not match the case 
+                    labelled 5, but it does match the case labelled 4, so as we intended, it prints:</p>
+                <p>The grade is B.</p>
+                <p>However,</p>
+                <p>The grade is C.</p>
+                <p>The grade is D.</p>
+                <p>The grade is E.</p>
+                <p>The grade is invalid</p>
+                <p>are also printed.</p>
+                <p>But why? The problem is a partial understanding of how the switch structure works. 
+                    Specifically when no break statement is included. After executing the statement 
+                    associated with the matching case label, execution continues with the statements 
+                    associated with the next case label. In this example, execution continues with all 
+                    the remaining statements in teh switch structure, resulting in the printing of four 
+                    unintended lines.</p>
+                <p>The correct program is as follows:</p>
+                <pre>
+{`
+switch (grade)
+{
+case 5:
+    System.out.println("The grade is A.");
+    break;
+case 4:
+    System.out.println("The grade is B.");
+    break;
+case 3:
+    System.out.println("The grade is C.");
+    break;
+case 2:
+    System.out.println("The grade is D.");
+    break;
+case 1:
+    System.out.println("The grade is F.");
+    break;
+default:
+    System.out.println("The grade is invalid");
+}
+`}
+                </pre>
+                <p>Believe it or not, this one kind of bug has gotten my coworkers and I into trouble in 
+                    more complex situations. It can be a tough one to spot and certainly offers a "doh" moment 
+                    for a season programmer</p>
+
             </div>
         );
     }
--- src/components/pages/cs-principles-page/subpages/task-six-subpage/TaskSixSubpage.js
+++ src/components/pages/cs-principles-page/subpages/task-six-subpage/TaskSixSubpage.js
@@ -6,8 +6,81 @@ class TaskSixSubpage extends Component {
     render() {
         return (
             <div>
-                <p>Develop, implement, and evaluate an algorithm for implementation in a program using
-                    appropriate mathematical and logical concepts, including abstraction to manage complexity</p>
+                <h1>Develop, implement, and evaluate an algorithm for implementation in a program using
+                    appropriate mathematical and logical concepts, including abstraction to manage complexity</h1>
+
+                <p>In my daily work, I use algorithms as a method of problem solving. Sometimes, I am able to use an 
+                    algorithm from a library that was imported or even from the coding language itself. One example of 
+                    that might be javascript's sort method. In javascript, I can sort an array using .sort(). The browser 
+                    determines which sorting algorithm is used, but I don't have to reinvent the wheel. Here's an example of 
+                    using that method in practice:</p>
+                    <pre>
+{`let fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.sort();
+//returns a sorted array for fruits
+console.log(fruits);
+//result
+Apple, Banana, Mango, Orange`}
+                    </pre>
+                <p>However, the algorithm is not always availible to me, so sometimes I need to create the algorithm. </p>
+                <p>Let's do that now, for simplicity's sake, let's create a new example problem</p>
+                <h2>Problem: Calculate the monthly paycheck of a salesperson at a local store</h2>
+                <ul>
+                    <li>Every salesperson has a base salary</li>
+                    <li>The salesperson recieves a bonus at the end of the month based on the following criteria:
+                        <ul>
+                            <li>If the person has been with the store for 5 years or less, the bonus is $10 for each 
+                                year he or she worked there.</li>
+                            <li>If the person has been with the store for more than 5 years, the bonus is $20 for each 
+                                year he or she has worked there.</li>
+                        </ul>
+                    </li>
+                    <li>The salesperson can recieve an additional bonus with the following criteria:
+                        <ul>
+                            <li>If the total sales made by the person for the month are more than $5,000 but less than $10,000, 
+                                he or she recieves 3% commission on the sale</li>
+                            <li>If the total sales made by the person for the month are atleast $10,000, 
+                                he or she recieves 6% commission on the sale</li>
+                        </ul>
+                    </li>
+                </ul>
+
+                <h2>Let's design an algorithm!</h2>
+                <p>Based on the above information, we need to know the base salary, the number of years the 
+                    person has been with the company, and the total sales made by that person for the month. Let's 
+                    create variables for the necessary values:</p>
+                <ul>
+                    <li>baseSalary</li>
+                    <li>noOfServiceYears</li>
+                    <li>totalSales</li>
+                    <li>additionalBonus</li>
+                </ul>
+
+                <p>The algorithm:</p>
+
+                <ol>
+                    <li>Get baseSalary</li>
+                    <li>Get noOfServiceYears</li>
+                    <li>
+                        <p>Calculate Bonus:</p>
+                        <p>if (noOfServiceYears is less than or equal to five)</p>
+                        <p>bonus = 10 * noOfServiceYears</p>
+                        <p>otherwise</p>
+                        <p>bonus = 20 * noOfServiceYears</p>
+                    </li>
+                    <li>Get total sales</li>
+                    <li>
+                        <p>Calculate additional bonus:</p>
+                        <p>if (totalSales is greater than 5000 and totalSales is less than 10000)</p>
+                        <p>additionalBonus = totalSales * (0.03)</p>
+                        <p>otherwise</p>
+                        <p>additionalBonus = totalSales * (0.06)</p>
+                    </li>
+                    <li>
+                        <p>Calculate paycheck:</p>
+                        <p>paycheck = baseSalary + bonus + additionalBonus</p>
+                    </li>
+                </ol>
             </div>
         );
     }
