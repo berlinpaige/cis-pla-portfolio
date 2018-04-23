6933f4e6a821fcfb76b2185b26c4800240c3e849
Berlin Sohn <berlinsohn@Berlins-MacBook-Pro.local>
2018-04-23 00:12:23 -0600

    clean up carousel file

--- src/components/pages/cs-principles-page/CarouselWrapper/CarouselWrapper.js
+++ src/components/pages/cs-principles-page/CarouselWrapper/CarouselWrapper.js
@@ -3,83 +3,45 @@ import './CarouselWrapper.css';
 import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext, DotGroup } from 'pure-react-carousel'
 import 'pure-react-carousel/dist/react-carousel.es.css';
 
-const CarouselWrapper = ({children}) => {
+const Carousel = ({children, height}) => {
+    return (
+        <CarouselProvider
+            naturalSlideWidth={50}
+            naturalSlideHeight={height}
+            totalSlides={children.length}
+            className="carouselProvider"
+        >        
+            <Slider className="carouselSlider">
+                {
+                    children.map((item, idx) => {
+                        return (
+                            <Slide key={idx} index={idx}>
+                                <div className="carouselTextWrapper">
+                                    {item}
+                                </div>
+                            </Slide>
+                        );
+                    })
+                }
+            </Slider>
+            <ButtonBack className="carouselBackButton">{`<`}</ButtonBack>
+            <ButtonNext className="carouselNextButton">{`>`}</ButtonNext>
+            <DotGroup className="carouselDots" />
+        </CarouselProvider>
+    )
+} 
+
+const CarouselWrapper = (props) => {
     return (
         <div>
             <div className="carouselWrapper isSmall">
-                <CarouselProvider
-                    naturalSlideWidth={50}
-                    naturalSlideHeight={70}
-                    totalSlides={children.length}
-                    className="carouselProvider"
-                >        
-                    <Slider className="carouselSlider">
-                        {
-                            children.map((item, idx) => {
-                                return (
-                                    <Slide key={idx} index={idx}>
-                                        <div className="carouselTextWrapper">
-                                            {item}
-                                        </div>
-                                    </Slide>
-                                );
-                            })
-                        }
-                    </Slider>
-                    <ButtonBack className="carouselBackButton">{`<`}</ButtonBack>
-                    <ButtonNext className="carouselNextButton">{`>`}</ButtonNext>
-                    <DotGroup className="carouselDots" />
-                </CarouselProvider>
+                <Carousel {...props} height={70}/>
             </div>
             <div className="carouselWrapper isMedium">
-                <CarouselProvider
-                    naturalSlideWidth={50}
-                    naturalSlideHeight={45}
-                    totalSlides={children.length}
-                    className="carouselProvider"
-                >        
-                    <Slider className="carouselSlider">
-                        {
-                            children.map((item, idx) => {
-                                return (
-                                    <Slide key={idx} index={idx}>
-                                        <div className="carouselTextWrapper">
-                                            {item}
-                                        </div>
-                                    </Slide>
-                                );
-                            })
-                        }
-                    </Slider>
-                    <ButtonBack className="carouselBackButton">{`<`}</ButtonBack>
-                    <ButtonNext className="carouselNextButton">{`>`}</ButtonNext>
-                    <DotGroup className="carouselDots" />
-                </CarouselProvider>
+                <Carousel {...props} height={45}/>
             </div>
             <div className="carouselWrapper isLarge">
-                <CarouselProvider
-                    naturalSlideWidth={50}
-                    naturalSlideHeight={20}
-                    totalSlides={children.length}
-                    className="carouselProvider"
-                >        
-                    <Slider className="carouselSlider">
-                        {
-                            children.map((item, idx) => {
-                                return (
-                                    <Slide key={idx} index={idx}>
-                                        <div className="carouselTextWrapper">
-                                            {item}
-                                        </div>
-                                    </Slide>
-                                );
-                            })
-                        }
-                    </Slider>
-                    <ButtonBack className="carouselBackButton">{`<`}</ButtonBack>
-                    <ButtonNext className="carouselNextButton">{`>`}</ButtonNext>
-                    <DotGroup className="carouselDots" />
-                </CarouselProvider>
+                <Carousel {...props} height={20}/>
             </div>
         </div>
     )
